8ca77639457a9cc3715840a599c8d1fdb572b5a7
wnstjd4778 <54669380+wnstjd4778@users.noreply.github.com>
2021-09-07 20:12:22 +0900

    BOJ

---
+++ 11050_이항 계수1.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int result = 1;
+        for(int i = n, j = 1; i > n - m; i--, j++) {
+            result *= i;
+            result /= j;
+        }
+        System.out.println(result);
+    }
+}
