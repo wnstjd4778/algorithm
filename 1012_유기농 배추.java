b0a39d564efacc9fdf8d2f58e57f2d2365706900
wnstjd4778 <54669380+wnstjd4778@users.noreply.github.com>
2021-09-13 19:02:50 +0900

    BOJ

---
+++ 1012_유기농 배추.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static int[][] arr;
+    public static boolean[][] checked;
+    public static int n, m, count;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(br.readLine());
+        while (size-- > 0) {
+            count = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            n = Integer.parseInt(st.nextToken());
+            m = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+
+            arr = new int[n + 1][m + 1];
+            checked = new boolean[n + 1][m + 1];
+            for(int i = 0; i < k; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                arr[x][y] = 1;
+            }
+
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (checked[i][j] == false && arr[i][j] == 1) {
+                        dfs(i, j);
+                        count++;
+                    }
+                }
+            }
+            System.out.println(count);
+        }
+    }
+
+    public static void dfs(int x, int y) {
+        checked[x][y] = true;
+        int[] dx = {0,1,0,-1};
+        int[] dy = {1,0,-1,0};
+        for(int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(nx >= 0 && nx < n && ny >= 0 && ny < m && !checked[nx][ny] && arr[nx][ny] == 1) {
+                dfs(nx,ny);
+            }
+        }
+    }
+}
