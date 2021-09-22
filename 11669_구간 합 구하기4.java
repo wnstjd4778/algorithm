f1fdeb029cc89fb5185635421cdc74a8329e4120
wnstjd4778 <54669380+wnstjd4778@users.noreply.github.com>
2021-09-22 18:14:52 +0900

    BOJ

---
+++ 11669_구간 합 구하기4.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n + 1];
+        for(int i = 1; i <= n; i++) {
+            int num = Integer.parseInt(st.nextToken());
+            arr[i] = arr[i - 1] + num;
+        }
+        for(int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            System.out.println(arr[y] - arr[x - 1]);
+        }
+    }
+}
