4eb2aa09df25a866fd2165279092d105ce539d2a
wnstjd4778 <54669380+wnstjd4778@users.noreply.github.com>
2021-09-26 11:20:40 +0900

    BOJ

---
+++ 1065_한수.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(solution(Integer.parseInt(br.readLine())));
+    }
+
+    public static int solution(int num) {
+        int cnt = 0;
+        if(num < 100) {
+            return num;
+        }
+
+        else {
+            cnt = 99;
+            if(num == 1000) {
+                num = 999;
+            }
+
+            for(int i = 100; i <= num; i++) {
+                int hun = i / 100;
+                int ten = (i / 10) % 10;
+                int one = i % 10;
+
+                if((hun - ten) == (ten - one)) {
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
