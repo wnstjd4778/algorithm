268a74644729603dde7b6f1deb46c244d74ce396
wnstjd4778 <54669380+wnstjd4778@users.noreply.github.com>
2021-09-26 11:54:48 +0900

    BOJ

---
+++ 11053_가장 큰 증가하는 부분 수열.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] seq = new int[n];
+        int[] dp = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            seq[i] = Integer.parseInt(st.nextToken());
+        }
+        for(int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for(int j = 0; j < i; j++) {
+                if(seq[j] < seq[i] && dp[i] < dp[j] + 1) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+        int max = -1;
+        for(int i = 0; i < n; i++) {
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+}
