1e47aeb6f3aeb6db36eb835b2d4db03e61e97fd8
wnstjd4778 <54669380+wnstjd4778@users.noreply.github.com>
2021-09-26 21:47:10 +0900

    BOJ

---
+++ 2440_별 찍기-3.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < n; i++) {
+            for(int j = n - i; j >= 1; j--) {
+                sb.append('*');
+            }
+            sb.append('\n');
+        }
+        System.out.println(sb);
+    }
+}
