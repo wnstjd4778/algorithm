6b595999cb607ab7fec82802e077875733282bbe
wnstjd4778 <54669380+wnstjd4778@users.noreply.github.com>
2021-09-29 17:18:48 +0900

    BOJ

---
+++ 1550_16진수.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        int b = Integer.parseInt(a, 16);
+        System.out.println(b);
+    }
+
+}
