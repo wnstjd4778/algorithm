81843adb037d14fe21d3ff6e651e0b9d574194a9
wnstjd4778 <54669380+wnstjd4778@users.noreply.github.com>
2021-10-02 14:16:53 +0900

    BOJ

---
+++ 2163_초콜렛 자르기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int result = (n - 1) + n * (m - 1);
+        System.out.println(result);
+    }
+}
+
+
