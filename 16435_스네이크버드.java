22fac85f5868570c32e7517ebe3cf680af80faf7
wnstjd4778 <54669380+wnstjd4778@users.noreply.github.com>
2021-10-10 11:39:48 +0900

    Create 16435_스네이크버드.java

---
+++ 16435_스네이크버드.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[] arr = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for(int i = 0; i < n; i++) {
+            if(m >= arr[i]) {
+                m++;
+            }
+        }
+        System.out.println(m);
+    }
+}
+
+
+
