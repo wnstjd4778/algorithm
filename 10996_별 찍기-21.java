dea1ee00cd5e19db146e9dcf290017803067e170
wnstjd4778 <54669380+wnstjd4778@users.noreply.github.com>
2021-10-30 22:26:20 +0900

    BOJ

---
+++ 10996_별 찍기-21.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < 2 * n; i++) {
+            for(int j = 0; j < n; j++) {
+                if((i + j) % 2 == 0) {
+                    System.out.print('*');
+                } else {
+                    System.out.print(' ');
+                }
+            }
+            System.out.println();
+        }
+    }
+}
