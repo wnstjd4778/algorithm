1762dad3cd36aba69e5967a1ebadcc7b31866a2d
wnstjd4778 <54669380+wnstjd4778@users.noreply.github.com>
2021-10-30 21:07:23 +0900

    BOJ

---
+++ 11719_그대로 출력하기2.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String str = sc.nextLine();
+            System.out.println(str);
+        }
+        sc.close();
+    }
+}
