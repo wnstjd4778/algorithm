cc06464f3356c5bbe33d05e98e5559b337cc4d7a
wnstjd4778 <54669380+wnstjd4778@users.noreply.github.com>
2021-11-01 00:43:09 +0900

    BOJ

---
+++ 2576_홀수.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < 7; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if(n % 2 == 1) {
+                sum += n;
+                min = Math.min(min, n);
+            }
+        }
+        if(sum == 0) {
+            System.out.println(-1);
+        } else {
+            System.out.println(sum);
+            System.out.println(min);
+        }
+    }
+}
