d643001c93e2dca4d6539b60121f3b4c7d0d465c
wnstjd4778 <54669380+wnstjd4778@users.noreply.github.com>
2021-11-09 22:23:00 +0900

    BOJ

---
+++ 9655_돌 게임.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        if(n % 2 == 1) {
+            System.out.println("SK");
+        } else {
+            System.out.println("CY");
+        }
+    }
+}
