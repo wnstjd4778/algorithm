6bfc1e3c32e09818e733ac6b1e5cd69c55b0dbc0
wnstjd4778 <54669380+wnstjd4778@users.noreply.github.com>
2021-11-15 19:35:20 +0900

    BOJ

--- 1915_가장 큰 정사각형.java
+++ 1915_가장 큰 정사각형.java
@@ -2,39 +2,24 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int n, m, ans;
-    static int[][] map, dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new int[n + 1][m + 1];
-        dp = new int[n + 1][m + 1];
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] dp = new int[n + 1][m + 1];
+        int max = 0;
         for(int i = 1; i <= n; i++) {
             String str = br.readLine();
             for(int j = 1; j <= m; j++) {
-                dp[i][j] = map[i][j] = str.charAt(j - 1) - '0';
-            }
-        }
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= m; j++) {
-                if(map[i][j] == 1) {
-                    dp[i][j] = check(i, j);
-                    ans = Math.max(ans, dp[i][j]);
+                dp[i][j] = str.charAt(j - 1) - '0';
+                if(dp[i][j] == 0) {
+                    continue;
                 }
+                dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
+                max = Math.max(max, dp[i][j]);
             }
         }
-        System.out.println(ans * ans);
-    }
-
-    static int check(int x, int y) {
-        int check1 = dp[x - 1][y - 1];
-        int check2 = dp[x][y - 1];
-        int check3 = dp[x - 1][y];
-        if(check1 == 0 || check2 == 0 || check3 == 0) {
-            return 1;
-        }
-        return Math.min(Math.min(check1, check2), check3) + 1;
+        System.out.println(max * max);
     }
 }
