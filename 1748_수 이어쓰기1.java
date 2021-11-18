ad31cc5f3c75a9a56370d835b984fe3ac3d5b1ff
wnstjd4778 <54669380+wnstjd4778@users.noreply.github.com>
2021-11-18 18:08:49 +0900

    BOJ

---
+++ 1748_수 이어쓰기1.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 1;
+        int temp = 0;
+        int length = 10;
+        for(int i = 1; i <= n; i++) {
+            if(i == length) {
+                cnt++;
+                length = length * 10;
+            }
+            temp += cnt;
+        }
+        System.out.println(temp);
+    }
+}
