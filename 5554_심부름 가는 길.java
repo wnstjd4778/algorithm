ff438cc6f230c3003d6700767c0b8f317b6c73b2
wnstjd4778 <54669380+wnstjd4778@users.noreply.github.com>
2021-11-20 23:02:39 +0900

    BOJ

---
+++ 5554_심부름 가는 길.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+        int e = a + b + c + d;
+        System.out.println(e / 60);
+        System.out.println(e % 60);
+    }
+
+
+}
